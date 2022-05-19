86f4a31033d3579ed8b45bd2ce634b926499f2b4
NyTsanta Andriamiarisoa <nytsanta.andriams@yahoo.fr>
2022-05-19 10:08:45 +0200

    message UTF-8 friendly

--- confirmation_mdp_oublie.php
+++ confirmation_mdp_oublie.php
@@ -24,7 +24,7 @@ $message = '
 
 // To send HTML mail, the Content-type header must be set
 $headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
 $headers .= 'From: OCRSQUARE <etglsquare@gmail.com>' . "\r\n";
 
 // renvoi true ou false (à ajuster quand il y aura accès à la base de donnée)
